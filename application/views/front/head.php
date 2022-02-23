f19fce527fbdfd5ebe1c74876788f1dea6bec2ba
KhoirunNimah <khoirunnimah117@gmail.com>
2022-02-23 13:50:59 +0700

    sistem ke 1

---
+++ application/views/front/head.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Khoirun Ni'mah</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/home/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/home/https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+
+    <!--header-->
+    <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <header>
+        <div class="container">
+            <h1><a href="index.html">Sistem Informasi Puskesmas Kerek</a></h1>
+            <ul>
+                <li class="active"><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="galeri.html">GALERI</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!--banner-->
+    <section class="banner">
+        <h2>WELCOME TO</h2>
+    </section>
+
+    <!-- about-->
+    <section class="about">
+        <div class="container">
+            <h3>ABOUT</h3>
+            <p><strong>Puskesmas Kerek</strong> merupakan Pelaksana Teknis Dinas Kesehatan Kabupaten Tuban yang bertanggung jawab terhadap pembangunan kesehatan pada wilayah kerja Kecamatan Kerek. Puskesmas Kerek mempunyai wilayah kerja 10 desa dari Kecamatan Kerek. Puskesmas Kerek siap melayani masyarakat Kerek sepenuh hati untuk mewujudkan masyarakat sehat dan sejahtera.</p>
+        </div>
+    </section>
+
+    <!--footer-->
+    <footer>
+        <div class="container">
+        <small>Copyright &copy; 2022 . Khoirun Ni'mah. All Right Reserved.</small>
+        </div>
+    </footer>
+
+</body>
+</html>
